3957a3840eab1e9ba2e292b289b554e5b5f873e2
User <user@Users-MacBook-Pro.local>
2020-01-29 09:26:18 +0000

    first commit

---
+++ simple_api/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', ( req, res ) => res.send([{
+    "name": "Xingang",
+    "producer": "Wa family members Zhaomei and Yenai",
+    "origin": "China",
+    "altitude": "1600masl",
+    "roast": "Light"
+  },
+  {
+    "name": "Reko Onancho",
+    "producer": "20 Reko Onancho Farmers",
+    "origin": "Ethiopia",
+    "altitude": "1950 - 2100masl",
+    "roast": "Light"
+  },
+  {
+    "name": "La Perla Del Cafe",
+    "producer": "Carlos Berrantes",
+    "origin": "Costa Rica",
+    "altitude": "1500 - 1650masl",
+    "roast": "Light"
+  },
+  {
+    "name": "La Virgen",
+    "producer": "100 Members of the Grupo Asociativo Villa Esperanza",
+    "origin": "Colombia",
+    "altitude": "1500 - 1900masl",
+    "roast": "Medium"
+  },
+  {
+    "name": "Karinga",
+    "producer": "Approx. 650 Karinga Co-operative Members",
+    "origin": "Kenya",
+    "altitude": "1700 - 1950masl",
+    "roast": "Medium"
+  }]))
+
+app.listen(8080, () => console.log("App running on 8080"))
